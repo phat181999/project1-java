b370cb98f2573f8ff502015e8f0b27c94f2b9a9f
phat181999 <phat181999@gmail.com>
2024-05-05 19:03:37 +0700

    add maven and edit pom

--- cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -14,7 +14,7 @@ import java.io.File;
 import java.time.Duration;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class NewCloudStorageApplicationTests {
+class CloudStorageApplicationTests {
 
     @LocalServerPort
     private int port;
